128773ebe08c636de48d30bf03e81a768523651e
MalContentFL <MalContentFL@gmail.com>
2018-02-04 22:07:09 -0500

    Initial Implementation
    Removed all methods and files pertaining to the previous method of calculating inputs.
    
    Google CSP doesn't allow external libraries. Resorting to AJAX requests instead.

--- script.js
+++ script.js
@@ -113,11 +113,7 @@ function calculate() {
     console.log("Input: " + input);
 
     // Make sure input is only numbers and operators
-    if (!allNumeric(input) || input == undefined) {
-        printResult("SYNTAX ERR");
-    } else {
-        input = characterSpecialCases(input);
-        input = evaluate(input);
+        input = math.eval(input);
 
         console.log("Output: " + input);
         printResult(input);
@@ -125,8 +121,6 @@ function calculate() {
         {
             localStorage.setItem("input", input);
         }
-        return true;
-    }
 }
 
 // Event Listener for the screen-text
@@ -142,88 +136,7 @@ function printResult(res) {
     document.getElementById("result").innerHTML = res;
 }
 
-// Inserts given String a given index, and asks to remove the char at index
-function insertStr(startingStr, insertStr, index) {
-    let beforeStr = startingStr.substring(0, index),
-        afterStr = startingStr.substring(index + 1);
-
-    console.log("NIndex: " + index);
-    console.log("Before String: " + beforeStr);
-    console.log("Insert String: " + insertStr);
-    console.log("After String: " + afterStr);
-
-    let finalStr = beforeStr + insertStr + afterStr;
-    console.log("Final String: " + finalStr);
-    return finalStr;
-}
-
 // ---------------CALCULATOR PAGE--------------- //
-
-function allNumeric(input) {
-    // Allowed characters
-    let numbers = /^[0-9,(,),+,\-,*,/,^,%,\.]+$/;
-    // Makes sure input only has allowed chars
-    if (input.match(numbers) || input === '') {
-        console.log("All Numeric!");
-        return true;
-    } else {
-        console.log("Not Numeric!");
-        return false;
-    }
-}
-
-function characterSpecialCases(input) {
-    // Do specific things for certain characters so it evaluates correctly
-    console.log(`Cleansing ${input} of ^ % ( )`);
-    // Index, Open and Closed Parenthesis Counts
-    let i = 0,
-        o = 0,
-        c = 0;
-    for (i = 0; i < input.length; i++) {
-        switch (input[i]) {
-            case "^":
-                console.log("^ at " + i);
-                input = insertStr(input, '**', i);
-                i += 2;
-                break;
-            case "(":
-                console.log("( at " + i);
-                if (input[i - 1] !== "(" && input[i - 1] !== "*") {
-                    input = insertStr(input, '*(', i);
-                }
-                i += 2;
-                o++;
-                break;
-            case ")":
-                console.log(") at " + i);
-                // Puts a * after a ) to avoid eval errors.
-                if ((input[i + 1] !== ")" && input[i + 1] !== "*") || i + 2 > input.length) {
-                    input = insertStr(input, ')*', i);
-                    i += 2;
-                }
-                // There is a eval error if there is a "))" evaluated, so add a *1 to avoid.
-                else if (input[i + 1] === ")") {
-                    input = insertStr(input, ")*1", i);
-                    i += 3;
-                }
-                c++;
-                break;
-            default:
-                console.log("No illegal char at " + i);
-                break;
-        }
-        console.log(`Input at index ${i}: ` + input);
-    }
-    // Removes extra ( or * leftover just incase to avoid eval/syntax error.
-    if (input.lastIndexOf("(") === input.length - 1 || input.lastIndexOf("*") === input.length - 1) {
-        input = input.substring(0, input.length - 1);
-    }
-    if (o != c) {
-        input = "SYNTAX ERR";
-    }
-    console.log("Done with Special Cases");
-    return input += ";";
-}
 
 // Gets keys into an array
 const keys = [].slice.call(document.getElementsByClassName("key"));
